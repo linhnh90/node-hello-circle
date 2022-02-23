2722637d977dfbdf562e0af3cd6b742df7789031
linhnh1990 <linh.nh112233445566@gmail.com>
2022-02-23 23:03:06 +0700

    update

--- python/index.py
+++ python/index.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
+
+app_env = os.getenv('APP_ENV')
+app_version = os.getenv('APP_VERSION')
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! Application version" + app_version "--Enviroment" + app_env
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
