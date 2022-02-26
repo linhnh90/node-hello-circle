419959d038def2c266d4a00702900acbff9599e7
linhnh1990 <linh.nh112233445566@gmail.com>
2022-02-26 22:57:57 +0700

    update

--- script_update_image/test.py
+++
@@ -1,5 +0,0 @@
-import docker
-
-image = "130228678771.dkr.ecr.ap-southeast-1.amazonaws.com/python:34"
-client = docker.from_env()
-client.containers.run(image, ports={'5000/tcp': 5000}, name='python', detach=True)
